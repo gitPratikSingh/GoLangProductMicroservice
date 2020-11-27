b8d029e8783c33b322ee2c8005343cc200c663a7
Pratik Singh <psingh22@ncsu.edu>
2020-11-27 14:02:16 -0800

    First commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/singhpratik/microservice/handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func(s *http.Server) {
+		fmt.Println("Listening for http calls")
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}(s)
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Kill)
+	signal.Notify(sigChan, os.Interrupt)
+
+	sig := <-sigChan
+	l.Printf("Received signal %s to terminate. trying to shutdown gracefully", sig)
+	tc, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	s.Shutdown(tc)
+}
